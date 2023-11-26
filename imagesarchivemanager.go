9f61748f725f82b584b1b1b615279ef23aed70bb
ashmeet28 <112308196+ashmeet28@users.noreply.github.com>
2023-11-26 08:50:00 -0500

    Added copy new files only op

--- imagesarchivemanager.go
+++ imagesarchivemanager.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"crypto/rand"
+	"crypto/sha256"
 	"encoding/hex"
+	"fmt"
 	"log"
 	"os"
 	"path"
@@ -23,6 +25,26 @@ func newImgTimePrefix() string {
 	return t[0:4] + t[5:7] + t[8:10]
 }
 
+func createHashMapForExistingFiles(d string) map[string]bool {
+	files, err := os.ReadDir(d)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	existingFilesHashMap := map[string]bool{}
+
+	for _, f := range files {
+		fName := f.Name()
+		data, err := os.ReadFile(path.Join(d, fName))
+		if err != nil {
+			log.Fatal(err)
+		}
+		sum := sha256.Sum256(data)
+		existingFilesHashMap[hex.EncodeToString(sum[0:32])] = true
+	}
+	return existingFilesHashMap
+}
+
 func main() {
 	op := os.Args[1]
 	dir1 := os.Args[2]
@@ -36,10 +58,10 @@ func main() {
 			log.Fatal(err)
 		}
 
-		for _, file := range files {
-			fName := file.Name()
+		for _, f := range files {
+			fName := f.Name()
 
-			newFName := newImgTimePrefix() + "_" + newImgId() + path.Ext(fName)
+			newFName := newImgTimePrefix() + "_" + newImgId() + "_0" + path.Ext(fName)
 
 			data, err := os.ReadFile(path.Join(dir1, fName))
 			if err != nil {
@@ -48,5 +70,27 @@ func main() {
 
 			os.WriteFile(path.Join(dir2, newFName), data, 0666)
 		}
+	case "copynewfilesonly":
+		existingFilesHashMap := createHashMapForExistingFiles(dir2)
+
+		files, err := os.ReadDir(dir1)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for _, f := range files {
+			fName := f.Name()
+			data, err := os.ReadFile(path.Join(dir1, fName))
+			if err != nil {
+				log.Fatal(err)
+			}
+			sum := sha256.Sum256(data)
+
+			if !existingFilesHashMap[hex.EncodeToString(sum[0:32])] {
+				os.WriteFile(path.Join(dir2, fName), data, 0666)
+			}
+		}
+	default:
+		fmt.Println("Invaild Operation")
 	}
 }
