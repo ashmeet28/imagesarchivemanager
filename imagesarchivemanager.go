70a91ed9e1d0790c079c9aa759bf81ce021e3f1b
ashmeet28 <112308196+ashmeet28@users.noreply.github.com>
2023-12-17 19:14:44 -0500

    Major update

--- imagesarchivemanager.go
+++ imagesarchivemanager.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"crypto/rand"
 	"crypto/sha256"
 	"encoding/hex"
 	"fmt"
@@ -9,151 +8,109 @@ import (
 	"os"
 	"path"
 	"strconv"
+	"strings"
 	"time"
 )
-
-func newImgId() string {
-	b := make([]byte, 16)
-	_, err := rand.Read(b)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return hex.EncodeToString(b)
-}
 
 func newImgTimePrefix() string {
 	t := time.Now().String()
 	return t[0:4] + t[5:7] + t[8:10]
 }
 
-func createHashMapForExistingFiles(d string) map[string]bool {
-	files, err := os.ReadDir(d)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	existingFilesHashMap := map[string]bool{}
-
-	for _, f := range files {
-		fName := f.Name()
-		data, err := os.ReadFile(path.Join(d, fName))
-		if err != nil {
-			log.Fatal(err)
-		}
-		sum := sha256.Sum256(data)
-		existingFilesHashMap[hex.EncodeToString(sum[0:32])] = true
-	}
-	return existingFilesHashMap
-}
-
 func main() {
 	op := os.Args[1]
 
 	switch op {
-	case "renamefiles":
-		dir1 := os.Args[2]
-		dir2 := os.Args[3]
+	case "archive":
+		filePathsArg := os.Args[2]
+		archiveDirArg := os.Args[3]
 
-		files, err := os.ReadDir(dir1)
-
-		if err != nil {
-			log.Fatal(err)
+		files, err_1 := os.ReadDir(archiveDirArg)
+		if err_1 != nil {
+			log.Fatal(err_1)
 		}
 
-		nameTimePrefix := newImgTimePrefix()
+		existingFilesHash := map[string]bool{}
 
 		for _, f := range files {
 			fName := f.Name()
-
-			newFName := nameTimePrefix + "_" + newImgId() + "_0" + path.Ext(fName)
-
-			data, err := os.ReadFile(path.Join(dir1, fName))
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			os.WriteFile(path.Join(dir2, newFName), data, 0666)
+			existingFilesHash[fName[9:25]] = true
 		}
-	case "copynewfilesonly":
-		dir1 := os.Args[2]
-		dir2 := os.Args[3]
 
-		existingFilesHashMap := createHashMapForExistingFiles(dir2)
+		data, err_2 := os.ReadFile(filePathsArg)
+		if err_2 != nil {
+			log.Fatal(err_2)
+		}
 
-		files, err := os.ReadDir(dir1)
-		if err != nil {
-			log.Fatal(err)
+		var withoutNewLinesData []byte
+
+		for _, c := range data {
+			if c == 0x0a {
+				withoutNewLinesData = append(withoutNewLinesData, 0x3a)
+			} else if c == 0x3a {
+				fmt.Println("error: found colon in file paths")
+				os.Exit(1)
+			} else {
+				withoutNewLinesData = append(withoutNewLinesData, c)
+			}
+		}
+
+		filePaths := strings.Split(string(withoutNewLinesData), ":")
+
+		var c1 int
+		var c2 int
+
+		for _, p := range filePaths {
+			ext := path.Ext(p)
+			if ext == ".jpeg" || ext == ".jpg" || ext == ".png" {
+				data, err := os.ReadFile(p)
+				if err != nil {
+					log.Fatal(err)
+				}
+				sum := sha256.Sum256(data)
+				sumHex := hex.EncodeToString(sum[0:8])
+				if !existingFilesHash[sumHex] {
+					os.WriteFile(path.Join(archiveDirArg, newImgTimePrefix()+"_"+sumHex+ext), data, 0666)
+					existingFilesHash[sumHex] = true
+					c2++
+				}
+				c1++
+			}
+		}
+		fmt.Println("Total files:", strconv.FormatInt(int64(len(filePaths)), 10))
+		fmt.Println("Images found:", strconv.FormatInt(int64(c1), 10))
+		fmt.Println("Images archived:", strconv.FormatInt(int64(c2), 10))
+
+	case "check":
+		archiveDirArg := os.Args[2]
+
+		files, err_1 := os.ReadDir(archiveDirArg)
+		if err_1 != nil {
+			log.Fatal(err_1)
 		}
 
 		var c int
 
 		for _, f := range files {
-			fName := f.Name()
-			data, err := os.ReadFile(path.Join(dir1, fName))
+			data, err := os.ReadFile(path.Join(archiveDirArg, f.Name()))
 			if err != nil {
 				log.Fatal(err)
 			}
 			sum := sha256.Sum256(data)
+			sumHex := hex.EncodeToString(sum[0:8])
 
-			if !existingFilesHashMap[hex.EncodeToString(sum[0:32])] {
-				os.WriteFile(path.Join(dir2, fName), data, 0666)
-				existingFilesHashMap[hex.EncodeToString(sum[0:32])] = true
+			if f.Name()[9:25] == sumHex {
+				fmt.Println("OK", f.Name())
+			} else {
+				fmt.Println("ERROR:", f.Name())
 				c++
 			}
+
 		}
 
-		fmt.Println("Copied " + strconv.FormatInt(int64(c), 10) + " files out of " + strconv.FormatInt(int64(len(files)), 10))
+		fmt.Println("Total files checked:", strconv.FormatInt(int64(len(files)), 10))
+		fmt.Println("Total errors:", strconv.FormatInt(int64(c), 10))
 
-	case "createmagickfile":
-		dir1 := os.Args[2]
-		dir2 := os.Args[3]
-
-		files, err := os.ReadDir(dir1)
-
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		fNamesIdHashMap := map[string]bool{}
-
-		for _, f := range files {
-			fName := f.Name()
-			fNamesIdHashMap[fName[9:43]] = true
-		}
-
-		var magickFileData []byte
-
-		for _, f := range files {
-			fName := f.Name()
-			if fName[42:43] == "0" && (!fNamesIdHashMap[fName[9:41]+"_1"]) {
-				magickFileData = append(magickFileData, []byte("magick"+" "+fName+" "+fName[0:41]+"_1.png")...)
-				magickFileData = append(magickFileData, 0x0a)
-			}
-		}
-
-		os.WriteFile(path.Join(dir2, "magickfile"), magickFileData, 0666)
-	case "createmvfile":
-		dir1 := os.Args[2]
-		dir2 := os.Args[3]
-
-		files, err := os.ReadDir(dir1)
-
-		if err != nil {
-			log.Fatal(err)
-		}
-		var d []byte
-		for _, f := range files {
-			fName := f.Name()
-			data, err := os.ReadFile(path.Join(dir1, fName))
-			if err != nil {
-				log.Fatal(err)
-			}
-			sum := sha256.Sum256(data)
-			newFName := fName[0:9] + hex.EncodeToString(sum[0:32])[0:16] + path.Ext(fName)
-			d = append(d, []byte("mv "+fName+" "+newFName)...)
-			d = append(d, 0xa)
-		}
-		os.WriteFile(path.Join(dir2, "mvfile"), d, 0666)
 	default:
 		fmt.Println("Invaild Operation")
 	}
